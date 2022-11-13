ef2504cda48ac65d8af3a531fd983cf2c6a9a088
AqilSoltanli <111847057+AqilSoltanli@users.noreply.github.com>
2022-11-13 23:19:33 +0400

    Add files via upload

---
+++ first js/js/app.js
@@ -0,0 +1,13 @@
+function Dark(){
+    var body = document.body;
+
+    if (body.style.backgroundColor==='black') {
+
+        body.style.backgroundColor='white'
+        body.style.color="black"
+    }
+    else{
+        body.style.backgroundColor='black'
+        body.style.color='white'
+    }
+}
